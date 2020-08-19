e7653bc5bd452c4f48908b314efe1cfaa2b889b8
Matheus Sampaio Queiroga <matheus@Matheus-DESKTOP.lan>
2020-08-19 00:12:45 -0300

    debug

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-reprepro $MORE --basedir "$WORKDIR_SH23/public" -C main includedeb $DIST package/main/*.deb &>> $WORKDIR_SH23/public/Reprepro_log.txt
-reprepro $MORE --basedir "$WORKDIR_SH23/public" -C contrib includedeb $DIST package/contrib/*.deb &>> $WORKDIR_SH23/public/Reprepro_log.txt
-reprepro $MORE --basedir "$WORKDIR_SH23/public" -C non-free includedeb $DIST package/non-free/*.deb &>> $WORKDIR_SH23/public/Reprepro_log.txt
-gpg --armor --output "$WORKDIR_SH23/public/pub.key" --export $KEY_ID
